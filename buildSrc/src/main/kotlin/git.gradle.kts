112bd1e7400660b83005646bd37a56bee695a00e
PineBale <272794187+PineBale@users.noreply.github.com>
2026-04-15 11:28:01 +0800

    placeholder

--- buildSrc/src/main/kotlin/git.gradle.kts
+++ buildSrc/src/main/kotlin/git.gradle.kts
@@ -2,7 +2,7 @@ var gitShaRevOutput = runProcessWithOutput("git log -1 --format=%h --abbrev=9 --
 
 if (gitShaRevOutput != "local") {
     val gitDiffProcess =
-        runProcess("git diff --quiet --ignore-space-at-eol --text ${project.projectDir.absolutePath}/src/main/kotlin/**/*.kt ${project.projectDir.absolutePath}/src/main/java/**/*.java ${project.projectDir.absolutePath}/*.gradle.kts ${project.projectDir.absolutePath}/gradle.properties ${project.projectDir.absolutePath}/src/main/resources/**")
+        runProcess("git diff --quiet --ignore-space-at-eol --text ${project.projectDir.absolutePath}/src/main/java/**/*.java ${project.projectDir.absolutePath}/*.gradle.kts ${project.projectDir.absolutePath}/gradle.properties ${project.projectDir.absolutePath}/src/main/resources/**")
     if (gitDiffProcess == 1) {
         project.logger.lifecycle("${project.name}: Uncommited changes detected.")
         gitShaRevOutput = "local"
